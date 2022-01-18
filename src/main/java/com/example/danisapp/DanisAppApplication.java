1c36761c66e690e8cc6f599151edf288f0c83da6
dan <dan.leo.iacob@gmail.com>
2022-01-17 18:44:05 -0800

    introduced name

--- src/main/java/com/example/danisapp/DanisAppApplication.java
+++ src/main/java/com/example/danisapp/DanisAppApplication.java
@@ -2,9 +2,7 @@ package com.example.danisapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
@@ -15,11 +13,11 @@ public class DanisAppApplication {
 		SpringApplication.run(DanisAppApplication.class, args);
 	}
 	@RestController
-	@RequestMapping("/getCurrentTime")
+	@RequestMapping("/getCurrentTime/{name}")
 	public class RoomController{
 		@GetMapping
-		public String rooms (){
-			return "Hey Dani! The current time is: " + LocalDateTime.now().toString();
+		public String rooms (@PathVariable String name){
+			return "Hey " + name + "! The current time is: " + LocalDateTime.now().toString();
 		}
 	}
 }
