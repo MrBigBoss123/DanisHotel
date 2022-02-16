dca95c1d5cfdb3dbc75a0f8fec1c22523d2fa6d0
dan <dan.leo.iacob@gmail.com>
2022-02-15 19:33:56 -0800

    validated email & phone Number & unit tests

--- src/main/java/com/example/danisapp/data/GuestValidator.java
+++ src/main/java/com/example/danisapp/data/GuestValidator.java
@@ -1,10 +1,20 @@
 package com.example.danisapp.data;
 
 public class GuestValidator {
+    static char[] validLetters = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '3', '4', '4', '5', '6', '7', '8', '9', '0'};
+
+    static char[] validEmailCharacters = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+            'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '3', '4', '4', '5', '6', '7', '8',
+            '9', '0', '@', '.','!', '#', '$', '%', '&', '*', '+', '-', '/', '=', '?', '^', '_', '`', '{', '|', '}', '~'};
+
+    static char[] validNumbers = new char[]{'1', '2', '3', '3', '4', '4', '5', '6', '7', '8', '9', '0'};
+
 
     public static boolean isEmailValid(String email) {
-        char[] validLetters = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-                'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '3', '4', '4', '5', '6', '7', '8', '9'};
+        if (email.length() <5){
+            return false;
+        }
         boolean validLetterFound = false;
         for (int i = 0; i < validLetters.length; i++){
             if ((email.toLowerCase()).charAt(0) == validLetters[i]) {
@@ -13,8 +23,54 @@ public class GuestValidator {
             }
         }
 
+        boolean allCharactersValid = true;
+        for (int i = 0; i<email.length(); i++){
+            boolean validCharacter =false;
+            for (int z = 0; z < validEmailCharacters.length; z++) {
+                if ((email.toLowerCase()).charAt(i) == validEmailCharacters[z]) {
+                    validCharacter = true;
+                    break;
+                }
+            }
+            if (validCharacter == false){
+                allCharactersValid = false;
+                break;
+            }
+        }
         boolean validAtFound = ((email.substring(0,email.length()-3)).contains("@"));
 
-        return validLetterFound && validAtFound;
+        String emailSubStringAfterAt = email.substring(email.indexOf("@")+1);
+        boolean invalidMultipleAtsFound = !emailSubStringAfterAt.contains("@");
+
+        boolean validDotFound = email.substring(email.indexOf("@")+1,email.length()-1).contains(".");
+
+
+        return validLetterFound && validAtFound && invalidMultipleAtsFound && validDotFound && allCharactersValid;
+    }
+    public static boolean isPhoneNumberValid(String phoneNumber) {
+        String clensedPhoneNumber = phoneNumber.trim()
+                .replace("(", "")
+                .replace(")", "")
+                .replace("-", "")
+                .replace("/", "")
+                .replace(" ", "");
+        if(clensedPhoneNumber.length()!= 10){
+            return false;
+        }
+
+
+        for (int i = 0; i<clensedPhoneNumber.length(); i++){
+            boolean validCharacter =false;
+            for (int z = 0; z < validNumbers.length; z++) {
+                if ((clensedPhoneNumber).charAt(i) == validNumbers[z]) {
+                    validCharacter = true;
+                    break;
+                }
+            }
+            if (validCharacter == false){
+               return false;
+            }
+        }
+        return true;
     }
 }
--- src/test/java/com/example/danisapp/data/GuestValidatorTest.java
+++ src/test/java/com/example/danisapp/data/GuestValidatorTest.java
@@ -8,21 +8,37 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GuestValidatorTest {
 
-    @Test
-    void testInvalidEmail() {
-        String invalidEmail = "notAValid email";
+
+    public static String[] invalidEmails() { return new String[]{ "", " ", " smith@gmail.com", "a@aaa","@gmail.com","a@a.", "dani@@gmail.com", "jeff;smith@gmail.com", "jeff.smith@gmail."};}
+
+    @ParameterizedTest
+    @MethodSource(value = "invalidEmails")
+    void testInvalidEmail(String invalidEmail) {
+        //String invalidEmail = "notAValid email";
         boolean valid = GuestValidator.isEmailValid(invalidEmail);
-        assertFalse(valid);
+        assertFalse(valid, invalidEmail);
     }
 
-    public static String[] validEmails() {
-        return new String[]{"john@yahoo.com", "bill@cz.net"};
-    }
+    public static String[] validEmails() { return new String[]{"John@yahoo.com", "bill@cz.net", "jeff.smith@gmail.com"};}
 
     @ParameterizedTest
     @MethodSource(value = "validEmails")
     void testValidEmails(String email) {
         boolean valid = GuestValidator.isEmailValid(email);
         assertTrue(valid);
+    }
+    public static String[] invalidPhoneNumbers() { return new String[]{"123456789", "", " ", "123;1243"};}
+    @ParameterizedTest
+    @MethodSource(value = "invalidPhoneNumbers")
+    void testInvalidPhoneNumbers(String phoneNumbers) {
+        boolean valid = GuestValidator.isPhoneNumberValid(phoneNumbers);
+        assertFalse(valid, phoneNumbers);
+    }
+    public static String[] validPhoneNumbers() { return new String[]{"123 123 1234", "123-123-1234", "(123) 123 1234", "(123)-123-1234"};}
+    @ParameterizedTest
+    @MethodSource(value = "validPhoneNumbers")
+    void testValidPhoneNumbers(String phoneNumbers) {
+        boolean valid = GuestValidator.isPhoneNumberValid(phoneNumbers);
+        assertTrue(valid, phoneNumbers);
     }
 }
